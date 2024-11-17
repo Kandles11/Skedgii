d7d181fd48f28ad83a58af2f175fe0362d6a3e9c
Colin Wong <cwongmath@gmail.com>
2024-11-17 00:07:54 -0600

    collect schedules

--- crx/background.js
+++ crx/background.js
@@ -6,11 +6,24 @@ const networkFilters = {
 
 const generateUriRegex = /^.*:\/\/utdallas\.collegescheduler\.com\/api\/terms\/[^\/]+\/schedules\/generate$/;
 
-browser.webRequest.onCompleted.addListener(async (details) => {
+browser.webRequest.onBeforeRequest.addListener((details) => {
     if (details.url.match(generateUriRegex)) {
+        let filter = browser.webRequest.filterResponseData(details.requestId);
+        let decoder = new TextDecoder("utf-8");
+        let encoder = new TextEncoder();
+
+        let str = "";
+        filter.ondata = (event) => {
+            str += decoder.decode(event.data);
+            filter.write(event.data);
+        };
+        filter.onstop = (event) => {
+            let obj = JSON.parse(str);
+            console.log(obj["schedules"]);
+            filter.disconnect();
+        }
+
         console.log("Hello background", details);
-        console.log("Cookies", await browser.cookies.getAll({ domain: "collegescheduler.com" }));
-    } else {
-        console.log("unrelated event");
+        // console.log("Cookies", await browser.cookies.getAll({ domain: "collegescheduler.com" }));
     }
-}, networkFilters);
+}, networkFilters, ["blocking"]);
