45951a972f1e06422ef4786de3053f76a7e249fd
Colin Wong <cwongmath@gmail.com>
2024-11-16 22:57:32 -0600

    use firefox :)

--- crx/background.js
+++ crx/background.js
@@ -6,10 +6,10 @@ const networkFilters = {
 
 const generateUriRegex = /^.*:\/\/utdallas\.collegescheduler\.com\/api\/terms\/[^\/]+\/schedules\/generate$/;
 
-chrome.webRequest.onCompleted.addListener(async (details) => {
+browser.webRequest.onCompleted.addListener(async (details) => {
     if (details.url.match(generateUriRegex)) {
         console.log("Hello background", details);
-        console.log("Cookies", await chrome.cookies.getAll({ domain: "collegescheduler.com" }));
+        console.log("Cookies", await browser.cookies.getAll({ domain: "collegescheduler.com" }));
     } else {
         console.log("unrelated event");
     }
