fb6cc3a92a4f6af62338dc96f0d6f3ed347dc914
Colin Wong <cwongmath@gmail.com>
2024-11-17 03:33:57 -0600

    successful crx post query to server

--- crx/background.js
+++ crx/background.js
@@ -6,6 +6,8 @@ const networkFilters = {
 
 const generateUriRegex = /^.*:\/\/utdallas\.collegescheduler\.com\/api\/terms\/[^\/]+\/schedules\/generate$/;
 
+const API_ENDPOINT = "http://100.65.237.99:8000/ratings/json-endpoint/";
+
 browser.webRequest.onBeforeRequest.addListener((details) => {
     if (details.url.match(generateUriRegex)) {
         let filter = browser.webRequest.filterResponseData(details.requestId);
@@ -15,15 +17,29 @@ browser.webRequest.onBeforeRequest.addListener((details) => {
         let str = "";
         filter.ondata = (event) => {
             str += decoder.decode(event.data);
-            filter.write(event.data);
         };
         filter.onstop = (event) => {
             let obj = JSON.parse(str);
-            console.log(obj["schedules"]);
-            filter.disconnect();
+            const toSend = obj["schedules"].map((value) => value["combination"]);
+            console.log(JSON.stringify(toSend));
+
+            let conn = new XMLHttpRequest();
+            conn.open("POST", API_ENDPOINT, false);
+            conn.setRequestHeader("Content-Type", "application/json");
+            conn.onreadystatechange = function() {
+                if (conn.readyState == 4 && conn.status == 200) {
+                    console.log("response", conn.response);
+
+                    filter.write(encoder.encode(str));
+                    filter.disconnect();
+                }
+            }
+            conn.send(JSON.stringify(toSend));
         }
 
         console.log("Hello background", details);
         // console.log("Cookies", await browser.cookies.getAll({ domain: "collegescheduler.com" }));
     }
+
+    return {};
 }, networkFilters, ["blocking"]);
